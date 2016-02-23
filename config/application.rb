65dd59779015dd38d9c680add6d4032656cd5aa2
Kouhei Sutou <kou@clear-code.com>
2016-02-23 10:14:06 +0900

    Use app/groonga

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,5 @@ module HerokuBlog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    config.autoload_paths += ["#{config.root}/lib"]
   end
 end
